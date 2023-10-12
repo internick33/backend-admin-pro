7921151a757a8a94af0e4b00d836fa668129028d
Jose Trinidad Garcia <internick33@gmail.com.com>
2023-10-11 18:14:25 -0600

    Seccion 12 - Google

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcryptjs');
 
 const Usuario = require('../models/usuario');
 const { generarJWT } = require('../helpers/jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 const login = async(req, res = response) => {
 
@@ -45,4 +46,46 @@ const login = async(req, res = response) => {
     }
 }
 
-module.exports = { login }
+const googleSignIn = async(req, res = response) => { 
+
+    try {
+        const { email, name, picture } = await googleVerify(req.body.token);
+
+        const usuarioDB = await Usuario.findOne({ email });
+
+        let usuario;
+
+        if(!usuarioDB){
+            usuario = new Usuario({
+                nombre: name,
+                email: email,
+                password: '@@@',
+                img: picture,
+                google: true
+            })
+        } else {
+            usuario = usuarioDB;
+            usuario.google = true;
+        }
+
+        //Guardar usuario
+        await usuario.save();
+
+         //Generar el token
+         const token = await generarJWT( usuario.id );
+        
+        res.json({
+            ok: true,
+            email, name, picture, token
+        })
+    } catch (error) {
+        console.log(error);
+
+        res.status(500).json({
+            ok: false,
+            msg: 'Token de google no es correcto'
+        });
+    }
+}
+
+module.exports = { login, googleSignIn }
